9faf78e572fc099969da01643aa34a97e914c4db
Rachaphol Plookaom <124332198+naiplawan@users.noreply.github.com>
2023-08-06 18:52:09 +0700

    fix:fix code in Sharebox.jsx

--- src/ShareBox.jsx
+++ src/ShareBox.jsx
@@ -11,13 +11,13 @@ function ShareBox() {
     return (
         <div className="share-box">
             <div className={`non-active-share ${isActive ? 'hidden' : ''}`} onClick={toggleActive}>
-                <img src="../public/images/icon-share.svg" alt="Share Icon"/>
+                <img src="/icon-share.svg" alt="Share Icon"/>
             </div>
             <div className={`active-share ${isActive ? 'visible' : ''}`}>
                 SHARE
-                <img src="../public/images/icon-facebook.svg" alt="Facebook Icon"/>
-                <img src="../public/images/icon-pinterest.svg" alt="Pinterest Icon"/>
-                <img src="../public/images/icon-twitter.svg" alt="Twitter Icon"/>
+                <img src="/icon-facebook.svg" alt="Facebook Icon"/>
+                <img src="/icon-pinterest.svg" alt="Pinterest Icon"/>
+                <img src="/icon-twitter.svg" alt="Twitter Icon"/>
             <div className="triangle-box"></div>
             </div>
         </div>
